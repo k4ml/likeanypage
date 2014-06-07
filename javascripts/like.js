d59a596d223e671e9868b7df3a09d3f1f166705c
Kamal Bin Mustafa <kamal.mustafa@gmail.com>
2014-06-08 06:15:26 +0800

    center everything

--- javascripts/like.js
+++ javascripts/like.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $('#generate').click(function(){
         var url = $('#url')[0].value;
-        var like_url = 'http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&show_faces=true&width=450&action=like&colorscheme=light';
-        var ifr = $('<iframe>', {src: like_url, height: '70px'})
+        var like_url = 'http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&show_faces=true&width=55&action=like&colorscheme=light&layout=box_count';
+        var ifr = $('<iframe>', {src: like_url, height: '70px', width: '60px', frameborder: '0', border: 'none', alloTransparency: 'true'})
         var display_url = $('<div>');
         display_url.append(url);
-        $('#ifr').empty().append(ifr).append(display_url);
+        $('#ifr').empty().append(ifr);
+        $('#display_url').append(display_url);
     });
 });
