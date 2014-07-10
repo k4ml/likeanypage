1e7c862d67618e608959466e3b136282c7f78275
k4ml <kamal.mustafa@gmail.com>
2014-07-10 09:29:51 +0800

    Update like.js

--- javascripts/like.js
+++ javascripts/like.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#generate').click(function(){
         var url = $('#url')[0].value;
-        var like_url = 'http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&show_faces=true&width=55&action=like&colorscheme=light&layout=box_count&appId=683362905063167';
+        var like_url = 'http://www.facebook.com/plugins/like.php?href='+encodeURIComponent(url)+'&show_faces=true&width=55&action=like&colorscheme=light&layout=standard&appId=683362905063167';
         var ifr = $('<iframe>', {src: like_url, height: '70px', width: '60px', frameborder: '0', border: 'none', alloTransparency: 'true'})
         var display_url = $('<div>');
         display_url.append(url);
